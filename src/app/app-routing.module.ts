d94642ee8e247f684b170d75895e16a81c3becf0
samjuri <soujanya_amjuri@epam.com>
2024-04-03 17:16:35 +0530

    Module 4

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,10 @@
 import { Routes } from '@angular/router';
+import { LoginFormComponent, RegistrationFormComponent } from './shared/components';
+import { CoursesListComponent } from './features/courses-list/courses-list.component';
 
 export const routes: Routes = [
-    /* Add your code here */
+   {path: 'login', component: LoginFormComponent}, 
+   {path: 'registration', component: RegistrationFormComponent},
+   {path: 'courses', component: CoursesListComponent}, 
+   {path: '**', component: CoursesListComponent}  
 ];
